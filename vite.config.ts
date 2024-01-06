fbc0aa6ddca6792428b62dd4d3a1e6932fdb7f3e
hikemalliday <117792777+hikemalliday@users.noreply.github.com>
2024-01-06 10:20:13 -0600

    docker compose folder added

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Define your environment variable
-
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: "0.0.0.0",
